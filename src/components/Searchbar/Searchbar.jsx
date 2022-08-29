a860b1d8ec7980aa8302cd1c9abbbd0a2f3df22f
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-29 05:12:44 +0300

    all

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,12 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+
 export const SearchBar = ({ onSubmit }) => (
   <header className="searchbar">
     <form className="searchForm" onSubmit={onSubmit}>
       <button type="submit" className="searchForm-button">
-        <span className="searchForm-button-label">Search</span>
+        <span className="searchForm-button-label">
+          <AiOutlineSearch className="searchForm-button-search-icon" />
+        </span>
       </button>
 
       <input
