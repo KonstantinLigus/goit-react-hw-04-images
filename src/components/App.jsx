7e108046b72a9b44969950685afa262912ff0e02
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-29 08:38:24 +0300

    resolve problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,19 +15,17 @@ export class App extends Component {
     isModalOpen: false,
     modalImage: '',
   };
-
+  totalPages = 0;
   componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
       this.setState({ isLoding: true });
-      fetchImages(this.state.query, this.state.page)
+      fetchImages(query, page)
         .then(images => {
           this.setState(prev => ({
-            images:
-              this.state.page === 1 ? images : [...prev.images, ...images],
+            images: page === 1 ? images.hits : [...prev.images, ...images.hits],
           }));
+          this.totalPages = images.totalHits;
         })
         .catch(error => console.log(error))
         .finally(() => this.setState({ isLoding: false }));
@@ -62,9 +60,11 @@ export class App extends Component {
           toggleModal={this.toggleModal}
         />
         {this.state.isLoding && <Loader />}
-        {this.state.images.length > 1 && !this.state.isLoding && (
-          <Button handleLoadMore={this.handleLoadMore} />
-        )}
+        {this.state.images.length > 0 &&
+          this.totalPages > 1 &&
+          !this.state.isLoding && (
+            <Button handleLoadMore={this.handleLoadMore} />
+          )}
         {this.state.isModalOpen && (
           <Modal
             modalImage={this.state.modalImage}
--- src/services/fetchImages.js
+++ src/services/fetchImages.js
@@ -5,5 +5,5 @@ export const fetchImages = async (guery, page) => {
   const images = await axios.get(
     `https://pixabay.com/api/?q=${guery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
-  return images.data.hits;
+  return images.data;
 };
