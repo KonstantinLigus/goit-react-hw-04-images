81e6b86bb8118588458c73532ba7879ae415b774
Konstantin Ligus <kostya1989com@gmail.com>
2022-09-06 13:23:57 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchImages } from 'services/fetchImages';
 import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -6,72 +6,59 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { SearchBar } from './Searchbar/Searchbar';
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoding: false,
-    query: '',
-    page: 1,
-    isModalOpen: false,
-    modalImage: '',
-  };
-  totalPages = 0;
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ isLoding: true });
-      fetchImages(query, page)
-        .then(images => {
-          this.setState(prev => ({
-            images: page === 1 ? images.hits : [...prev.images, ...images.hits],
-          }));
-          this.totalPages = images.totalHits;
-        })
-        .catch(error => console.log(error))
-        .finally(() => this.setState({ isLoding: false }));
-    }
-  }
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoding, setIsLoding] = useState(false);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  let totalPages = 0;
 
-  onSubmit = e => {
+  useEffect(() => {
+    setIsLoding(true);
+    fetchImages(query, page)
+      .then(images => {
+        setImages(prev =>
+          page === 1 ? images.hits : [...prev, ...images.hits]
+        );
+        totalPages = images.totalHits;
+      })
+      .catch(error => console.log(error))
+      .finally(() => setIsLoding(false));
+  }, [query, page]);
+
+  const onSubmit = e => {
     e.preventDefault();
-    this.setState({ query: e.target.elements.query.value, page: 1 });
+    setQuery(e.target.elements.query.value);
+    setPage(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prev => ({
-      page: prev.page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
-  toggleModal = image => {
+  const toggleModal = image => {
     if (image) {
-      this.setState({ isModalOpen: true, modalImage: image });
+      setIsModalOpen(true);
+      setModalImage(image);
       return;
     }
-    this.setState({ isModalOpen: false, modalImage: '' });
+    setIsModalOpen(false);
+    setModalImage('');
   };
 
-  render() {
-    return (
-      <div className="app">
-        <SearchBar onSubmit={this.onSubmit} />
-        <ImageGallery
-          images={this.state.images}
-          toggleModal={this.toggleModal}
-        />
-        {this.state.isLoding && <Loader />}
-        {this.state.images.length > 0 &&
-          this.totalPages > 1 &&
-          !this.state.isLoding && (
-            <Button handleLoadMore={this.handleLoadMore} />
-          )}
-        {this.state.isModalOpen && (
-          <Modal
-            modalImage={this.state.modalImage}
-            toggleModal={this.toggleModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <SearchBar onSubmit={onSubmit} />
+      <ImageGallery images={images} toggleModal={toggleModal} />
+      {isLoding && <Loader />}
+      {images.length > 0 && totalPages > 1 && !isLoding && (
+        <Button handleLoadMore={handleLoadMore} />
+      )}
+      {isModalOpen && (
+        <Modal modalImage={modalImage} toggleModal={toggleModal} />
+      )}
+    </div>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,22 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.onESCclick);
-  }
-
-  onESCclick = e => {
-    if (e.key === 'Escape') {
-      this.props.toggleModal();
-    }
-  };
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.onESCclick);
-  }
-
-  render() {
-    const { modalImage, toggleModal } = this.props;
-    return (
-      <div className="overlay" onClick={() => toggleModal()}>
-        <div className="modal">
-          <img src={modalImage} alt="" />
-        </div>
+export const Modal = ({ modalImage, toggleModal }) => {
+  useEffect(() => {
+    const onESCclick = e => {
+      if (e.key === 'Escape') {
+        toggleModal();
+      }
+    };
+    document.addEventListener('keydown', onESCclick);
+    return () => {
+      document.removeEventListener('keydown', onESCclick);
+    };
+  }, [toggleModal]);
+  return (
+    <div className="overlay" onClick={() => toggleModal()}>
+      <div className="modal">
+        <img src={modalImage} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
